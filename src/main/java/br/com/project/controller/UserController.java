443452c44a7464a7fd5a1cc46685f6dd3247344a
Renan Barbioni <renanbarbioni@gmail.com>
2014-11-30 23:10:27 -0200

    Initial commit

--- src/main/java/br/com/project/controller/UserController.java
+++ src/main/java/br/com/project/controller/UserController.java
@@ -2,8 +2,6 @@ package br.com.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
